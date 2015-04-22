3801cdfb834bf3e40e086162406a919758dec5b7
Andrew Aldridge <i80and@foxquill.com>
2015-04-22 16:42:49 -0400

    Upgrade firefox to 37.0.2

--- firefox/vendor.js
+++ firefox/vendor.js
@@ -7,3 +7,5 @@ pref("browser.shell.checkDefaultBrowser", false);
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
+
+pref('app.update.enabled', false)
